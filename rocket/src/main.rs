22946da5e492e69236d12f58a1c60c190975e71e
develon2015 <302615249@qq.com>
2023-03-21 17:56:32 +0800

    fs

--- rocket/src/main.rs
+++ rocket/src/main.rs
@@ -1,6 +1,8 @@
 #[macro_use] extern crate rocket;
 use rocket::State;
 use rocket::fairing::AdHoc;
+use rocket::fs::FileServer;
+use rocket::fs::relative;
 use rocket::serde::Deserialize;
 mod startup;
 
@@ -12,23 +14,40 @@ struct AppConfig {
     version: String,
 }
 
+#[get("/")]
+async fn index() -> String {
+    format!("Hello")
+}
+
 #[get("/version")]
 async fn version(config: &State<AppConfig>) -> String {
     format!("server running at {}:{}, version: {}", config.address, config.port, config.version)
     // "version 1.0".to_string()
 }
 
+fn fs() -> AdHoc {
+    AdHoc::on_ignite("准备点火", |rocket| async move {
+        rocket.mount("/fs", FileServer::from(relative!("static")))
+            .attach(AdHoc::on_request("onRequest", |req, data| {
+                Box::pin(async move {
+                    println!("{} {} {}", req.method(), req.uri(), req.accept().unwrap().first().unwrap().to_string());
+                })
+            }))
+    })
+}
+
 async fn go() -> Result<(), rocket::Error> {
     let app = startup::start_with_figment();
     // let app = startup::start_with_default_config();
-    let app = app.mount("/", routes![version]);
+    let app = app.mount("/", routes![index, version]);
     let app = app.attach(AdHoc::config::<AppConfig>());
+    let app = app.attach(fs());
     let _ = app.launch().await?;
     println!("Shutdown!");
     Ok(())
 }
 
-#[tokio::main]
+#[rocket::main]
 async fn main() -> Result<(), rocket::Error> {
     go().await
 }
