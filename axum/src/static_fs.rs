3b00dbc4203caf1294f3d4312c70bf683e5191c6
develon2015 <302615249@qq.com>
2023-04-07 17:57:36 +0800

    detach

--- axum/src/static_fs.rs
+++ axum/src/static_fs.rs
@@ -15,32 +15,45 @@ fn app() -> axum::Router {
 
 // #[tokio::main(flavor = "multi_thread", worker_threads = 5)]
 #[tokio::main(flavor = "current_thread")]
-async fn entry() {
-    let port = 8080;
-    #[cfg(windows)]
-    unsafe {
-        WinExec(format!("explorer http://127.0.0.1:{}\0", port).as_ptr(), 0);
-        FreeConsole();
-        AllocConsole();
-    }
-    let server = axum::Server::bind(&format!("0.0.0.0:{}", port).parse().unwrap())
+async fn entry(socket: std::net::TcpListener) {
+
+    let server = axum::Server::from_tcp(socket).unwrap()
         .serve(app().into_make_service_with_connect_info::<std::net::SocketAddr>());
     println!("server run on: {}", server.local_addr());
     server.await.unwrap();
 }
 
 #[cfg(windows)]
+fn start() {
+    let port = 8080;
+    unsafe {
+        WinExec(format!("explorer http://127.0.0.1:{}\0", port).as_ptr(), 0);
+        FreeConsole();
+        AllocConsole();
+    }
+    match std::net::TcpListener::bind(format!("0.0.0.0:{}", port)) {
+        Ok(socket) => {
+            entry(socket);
+        }
+        Err(e) => {
+            println!("{}", e);
+        }
+    }
+}
+
+#[cfg(windows)]
 fn main() {
-    let exe = std::env::current_exe().unwrap();
     match std::env::var("detatch") {
         Ok(v) if v == "1" => {
-            let mut a = std::process::Command::new(exe);
-            a.arg("detatch");
-            a.env("detatch", "1");
-            a.spawn().unwrap();
+        }
+        _ => {
+            let exe = std::env::current_exe().unwrap();
+            let mut process = std::process::Command::new(exe);
+            process.arg("detatch");
+            process.env("detatch", "1");
+            process.spawn().unwrap();
             return;
         },
-        _ => ()
     }
-    entry();
+    start();
 }
